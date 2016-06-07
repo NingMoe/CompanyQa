216f00551ee06367c4e4b0d8361222d2820925db
lihuiyan <lihuiyan@koolearn.com>
2016-06-07 18:26:57 +0800

    1.修改发送邮件配置 2.修改测试计划空行bug

--- src/main/java/com/koolearn/qa/util/MppUtil.java
+++ src/main/java/com/koolearn/qa/util/MppUtil.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import com.koolearn.qa.constant.Constant;
 import net.sf.mpxj.*;
 import net.sf.mpxj.mpp.MPPReader;
+import org.apache.commons.lang.StringUtils;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
@@ -21,15 +22,15 @@ public class MppUtil {
 
     SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
 
-    public static String dumpJson(String mppFilePath){
+    public static String dumpJson(String mppFilePath) {
         MppUtil mappUtil = new MppUtil();
         ProjectFile projectFile = mappUtil.readProject(mppFilePath);
-        if(projectFile == null){
+        if (projectFile == null) {
             return null;
         }
         List<TreeGridModel> list = mappUtil.dumpTreeList(projectFile);
         String json = null;
-        if(list!=null) {
+        if (list != null) {
             Gson gson = new Gson();
             json = gson.toJson(list);
         }
@@ -60,54 +61,60 @@ public class MppUtil {
     public String getResource(Task task) {
         StringBuffer buf = new StringBuffer();
         List<ResourceAssignment> assignments = task.getResourceAssignments();
-        for (ResourceAssignment assignment : assignments)
-        {
+        for (ResourceAssignment assignment : assignments) {
             Resource resource = assignment.getResource();
             if (resource != null) {
                 buf.append(resource.getName()).append(Constant.COMMA);
             }
         }
-        if(buf.toString().endsWith(Constant.COMMA)){
-            return buf.substring(0,buf.length()-1);
+        if (buf.toString().endsWith(Constant.COMMA)) {
+            return buf.substring(0, buf.length() - 1);
         }
         return buf.toString();
     }
 
     /**
-     *
      * 获取前置任务
+     *
      * @param task
      * @return
      */
-    public String getPredecessors (Task task){
+    public String getPredecessors(Task task) {
         StringBuffer buf = new StringBuffer();
         List<Relation> list = task.getPredecessors();
-        if(list!=null && list.size()>0){
-            for(int i=0; i<list.size(); i++){
+        if (list != null && list.size() > 0) {
+            for (int i = 0; i < list.size(); i++) {
                 Relation relation = list.get(i);
                 buf.append(relation.getTargetTask().getID()).append(Constant.COMMA);
             }
         }
-        if(buf.toString().endsWith(Constant.COMMA)){
-            return buf.substring(0,buf.length()-1);
+        if (buf.toString().endsWith(Constant.COMMA)) {
+            return buf.substring(0, buf.length() - 1);
         }
         return buf.toString();
     }
 
     public List<TreeGridModel> dumpTreeList(ProjectFile file) {
         List childTasks = file.getChildTasks();
-        if(childTasks!=null){
+        if (childTasks != null) {
             for (int i = 0; i < childTasks.size(); i++) {
                 Task task = (Task) childTasks.get(i);
                 TreeGridModel treeGridModel = new TreeGridModel();
-                if(task.getID()!=null && task.getID()!=0) {
+                if (task.getID() != null && task.getID() != 0) {
                     treeGridModel.id = task.getID();
                     treeGridModel.pId = null;
+                    if (StringUtils.isBlank(task.getName())) {
+                        continue;
+                    }
                     treeGridModel.name = task.getName();
-                    if(task.getManualDuration()!=null){
-                        treeGridModel.duration =task.getManualDuration().getDuration();
-                    }else{
-                        treeGridModel.duration =task.getDuration().getDuration();
+                    if (task.getManualDuration() != null) {
+                        treeGridModel.duration = task.getManualDuration().getDuration();
+                    } else {
+                        if (task.getDuration() != null) {
+                            treeGridModel.duration = task.getDuration().getDuration();
+                        } else {
+                            treeGridModel.duration = null;
+                        }
                     }
                     treeGridModel.start = formatter.format(task.getStart());
                     treeGridModel.finish = formatter.format(task.getFinish());
@@ -115,7 +122,7 @@ public class MppUtil {
                     treeGridModel.resource = getResource(task);
                     treeGridModel.children = hierarchy(task);
                     treeGridList.add(treeGridModel);
-                }else{
+                } else {
                     treeGridList = hierarchy(task);
                 }
             }
@@ -123,8 +130,8 @@ public class MppUtil {
         return treeGridList;
     }
 
-    private List<TreeGridModel> hierarchy(Task task){
-        List<TreeGridModel> list=new ArrayList<>();
+    private List<TreeGridModel> hierarchy(Task task) {
+        List<TreeGridModel> list = new ArrayList<>();
         List childTasks = task.getChildTasks();
         for (int i = 0; i < childTasks.size(); i++) {
             Task child = (Task) childTasks.get(i);
@@ -132,11 +139,13 @@ public class MppUtil {
             treeGridModel.id = child.getID();
             treeGridModel.pId = task.getID();
             treeGridModel.name = child.getName();
-
-            if(child.getManualDuration()!=null){
-                treeGridModel.duration =child.getManualDuration().getDuration();
-            }else{
-                treeGridModel.duration =child.getDuration().getDuration();
+            if (StringUtils.isBlank(task.getName())) {
+                continue;
+            }
+            if (child.getManualDuration() != null) {
+                treeGridModel.duration = child.getManualDuration().getDuration();
+            } else {
+                treeGridModel.duration = child.getDuration().getDuration();
             }
             treeGridModel.start = formatter.format(child.getStart());
             treeGridModel.finish = formatter.format(child.getFinish());
@@ -148,14 +157,14 @@ public class MppUtil {
         return list;
     }
 
-    public class TreeGridModel{
+    public class TreeGridModel {
         public Integer id;
         public Integer pId;
         public String name;
         public Double duration;
         public String start;
         public String finish;
-        public String beforeTask ;
+        public String beforeTask;
         public String resource;
         public List<TreeGridModel> children;
     }
